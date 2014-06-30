d4f36136f48239a6296f99a94b9653fa77267240
Coen Jacobs <coenjacobs@gmail.com>
2014-06-30 10:00:12 +0200

    Added bootstrapping like setup in main class

--- includes/class-wc-naguro.php
+++ includes/class-wc-naguro.php
@@ -1,18 +1,35 @@
 <?php
 
 class WC_Naguro {
-	/** @var \Naguro */
-	private $naguro;
+	/** @var Naguro_Handler_Factory */
+	public $handler_factory;
+
+	/** @var Naguro_Model_Repository */
+	public $model_repository;
 
 	public function __construct() {
-		include_once( 'naguro-library/naguro.php' );
-		$this->naguro = new Naguro();
+		$this->always_include();
 
-		$this->includes();
-		$this->naguro->handler_factory->register_api_handler( new WP_API_Handler() );
+		$this->handler_factory = new Naguro_Handler_Factory();
+		$this->model_repository = new Naguro_Model_Repository();
+
+		$this->setup_handler();
 	}
 
-	private function includes() {
+	/**
+	 * Contains includes that should be included in every single request
+	 */
+	private function always_include() {
+		include_once( 'naguro-library/application/interfaces/interface-api-handler.php' );
+		include_once( 'naguro-library/application/handler-factory.php' );
+		include_once( 'naguro-library/application/model-repository.php' );
+	}
+
+	/**
+	 * Setup the WordPress specific API request handler
+	 */
+	private function setup_handler() {
 		include( 'class-wp-api-handler.php' );
+		$this->handler_factory->register_api_handler( new WP_API_Handler() );
 	}
 }
