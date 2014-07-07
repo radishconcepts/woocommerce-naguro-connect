55c88de084e81a456a31288fc5b5c105b582dffd
Coen Jacobs <coenjacobs@gmail.com>
2014-07-07 11:28:59 +0200

    Panel for Naguro product specific settings

--- includes/admin/class-wc-naguro-product-admin.php
+++ includes/admin/class-wc-naguro-product-admin.php
@@ -5,10 +5,16 @@
  * Responsible for settings related to products in the administration panel
  */
 class WC_Naguro_Product_Admin {
+	/** @var \WC_Naguro_Settings_Panel */
+	private $product_panel;
+
 	public function __construct() {
 		add_action( 'woocommerce_process_product_meta', array( $this, 'save' ), 10, 2 );
 
 		add_filter( 'product_type_options', array( $this, 'add_product_type' ), 10, 1 );
+
+		include( NAGURO_PLUGIN_PATH . 'includes/admin/class-wc-naguro-settings-panel.php' );
+		$this->product_panel = new WC_Naguro_Settings_Panel();
 	}
 
 	public function save( $post_id, $post ) {
---
+++ includes/admin/class-wc-naguro-settings-panel.php
@@ -0,0 +1,29 @@
+<?php
+
+class WC_Naguro_Settings_Panel {
+	public function __construct() {
+		add_filter( 'woocommerce_product_data_tabs', array( $this, 'product_data_tabs' ), 10, 1 );
+
+		add_action( 'woocommerce_process_product_meta', array( $this, 'save_panel_settings' ), 10, 1 );
+		add_action( 'woocommerce_product_data_panels', array( $this, 'product_data_panels' ), 10, 0 );
+	}
+
+	public function product_data_tabs( $tabs ) {
+		$tabs['purchase_information'] = array(
+			'label'  => __( 'Naguro', 'woocommerce-naguro-connect' ),
+			'target' => 'woocommerce_naguro_settings',
+			'class'  => array( 'show_if_naguro' ),
+		);
+
+		return $tabs;
+	}
+
+	public function save_panel_settings( $post_id ) {
+	}
+
+	public function product_data_panels() {
+		echo '<div id="woocommerce_naguro_settings" class="panel woocommerce_options_panel show_if_naguro">';
+
+		echo '</div>';
+	}
+}
