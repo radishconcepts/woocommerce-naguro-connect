33e9a0235d48a4bc815cf737856a301e640adc79
Coen Jacobs <coenjacobs@gmail.com>
2014-07-07 10:18:23 +0200

    Radisch. Dus.

--- woocommerce-naguro-connect.php
+++ woocommerce-naguro-connect.php
@@ -4,7 +4,7 @@
  * Plugin Name: WooCommerce Naguro Connect
  * Description: Connects your WooCommerce powered store to Naguro.
  * Version: 0.0.1-beta-alpha
- * Author: Radisch Concepts
+ * Author: Radish Concepts
  * Author URI: http://radishconcepts.com
  */
 
