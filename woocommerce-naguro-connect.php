0af6bd23cdd7b9ee3c482fbbf78085d5d47cb210
Coen Jacobs <coenjacobs@gmail.com>
2014-09-15 10:50:09 +0200

    PHP 5.2 compatible autoloading

--- woocommerce-naguro-connect.php
+++ woocommerce-naguro-connect.php
@@ -19,8 +19,14 @@ function wc_naguro_connect_get_instance() {
 add_action('init', 'wc_naguro_connect_init');
 
 function wc_naguro_connect_init() {
-	include('vendor/autoload.php');
+	// Load PHP 5.2 compatible autoloader if required
+	if (version_compare(PHP_VERSION, '5.3.0') >= 0) {
+		include('vendor/autoload.php');
+	} else {
+		include('vendor/autoload_52.php');
+	}
 
+	// Setup the core class instance
 	global $wc_naguro_connect;
 	$wc_naguro_connect = wc_naguro_connect_get_instance();
 }
