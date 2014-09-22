0dcdb8ce6cf0286c4021c23c20a1d7d9e122c957
Gaya Kessler <gaya.kessler@gmail.com>
2014-09-22 16:41:23 +0200

    Add background image stub

--- includes/stubs/NaguroStubs.php
+++ includes/stubs/NaguroStubs.php
@@ -7,7 +7,8 @@ class NaguroStubs {
 				"name"              => "Front",
 				"size_description"  => "25mm x 12.3mm",
 				"output_width"      => 25,
-				"output_height"     => 12.3
+				"output_height"     => 12.3,
+				"product_image"     => NAGURO_PLUGIN_URL . "/includes/stubs/background.png"
 			)
 		);
 	}
