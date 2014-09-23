5a8830a9f8e03f274429692fceac2fe5b063eaa1
Gaya Kessler <gaya.kessler@gmail.com>
2014-09-23 10:07:25 +0200

    Bind opening of tab to init of imgarea

--- assets/js/wc-naguro.js
+++ assets/js/wc-naguro.js
@@ -1,7 +1,24 @@
 (function ($) {
-    $(document).ready(function () {
-        $('.naguro-printable-product img').imgAreaSelect({
-            handles: true
+    $(window).load(function () {
+        $('ul.wc-tabs a').click(function(){
+           if (this.href.indexOf("woocommerce_naguro_settings") !== -1) {
+                //THANKS WC!
+                setTimeout(init_imgselectarea, 50);
+           }
         });
     });
+
+    function init_imgselectarea() {
+        $('.naguro-printable-product').each(function () {
+            var obj = $(this);
+            var img = obj.find("img");
+            var imgWidth = img.width();
+            var imgHeight = img.height();
+
+
+            img.imgAreaSelect({
+                handles: true
+            });
+        });
+    }
 })(jQuery);
