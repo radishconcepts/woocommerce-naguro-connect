682e2b83643f226bd6fd991a7a564050ae1763c7
Coen Jacobs <coenjacobs@gmail.com>
2014-10-02 11:01:10 +0200

    Replace dummy design area data with real data

--- includes/core/requests/class-wc-naguro-session-get-request.php
+++ includes/core/requests/class-wc-naguro-session-get-request.php
@@ -1,40 +1,67 @@
 <?php
 
 class WC_Naguro_Session_Get_Request extends WC_Naguro_Request {
+	private function get_product() {
+		return wc_get_product( $this->session->get('product_id'));
+	}
+
+	/**
+	 * @param $product WC_Product
+	 * @return array
+	 */
+	private function get_design_areas( $product ) {
+		return get_post_meta($product->id, 'naguro_design_area', false);
+	}
+
+	private function get_product_data_array() {
+		$product = $this->get_product();
+		$design_areas = $this->get_design_areas( $product );
+
+		$design_areas_array = array();
+		$subtypes_array = array();
+		$i = 1;
+		foreach ( $design_areas as $design_area ) {
+			$image_src = wp_get_attachment_image_src( $design_area['product_image_id'], 'full' );
+			$image_src = $image_src[0];
+
+			$design_areas_array[] = array(
+				'product_design_area_id' => $i,
+				'name' => $design_area['name'],
+				'sort_order' => $i,
+			);
+
+			$subtypes_array[] = array(
+				'product_subtype_id' => $i,
+				'product_design_area_id' => $i,
+				'is_circle' => false, // @todo implementation plz
+				'src_width' => $design_area['output_width'],
+				'src_height' => $design_area['output_height'],
+				'print_width' => $design_area['print_width'],
+				'print_height' => $design_area['print_height'],
+				'left' => $design_area['left'],
+				'top' => $design_area['top'],
+				'size_description' => $design_area['size_description'],
+				'image_src' => $image_src,
+				'overlay_src' => '',
+			);
+
+			$i++;
+		}
+
+		return array(
+			'product_id' => $product->id,
+			'name' => $product->get_title(),
+			'design_areas' => $design_areas_array,
+			'subtypes' => $subtypes_array,
+		);
+	}
+
 	public function output() {
 		$object = new StdClass();
 		$object->session_id = $this->session->get_id();
 		$object->locale = 'nl_NL';
 		$object->start_product_subtype_id = 1;
-		$object->product = array(
-			'product_id' => 1,
-			'name' => 'USB stick',
-			'design_areas' => array(
-				array(
-					'product_design_area_id' => 1,
-					'name' => 'Front',
-					'sort_order' => 1
-				),
-			),
-			'subtypes' => array(
-				array(
-					'product_subtype_id' => 1,
-					'product_design_area_id' => 1,
-					'is_circle' => false,
-					'src_width' => 480,
-					'src_height' => 200,
-					'print_width' => 51.04166666,
-					'print_height' => 48,
-					'output_width' => 244.8,
-					'output_height' => 96,
-					'left' => 25.625,
-					'top' => 24.5,
-					'size_description' => "1,5cm x 0.8cm",
-					'image_src' => "/images/products/background.png",
-					'overlay_src' => "",
-				),
-			),
-		);
+		$object->product = $this->get_product_data_array();
 
 		echo json_encode( $object ); die();
 	}
