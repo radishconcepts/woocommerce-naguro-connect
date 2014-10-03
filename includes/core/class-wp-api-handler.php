6a8ccd216ff9380b02e29ec3547475d063cec515
Coen Jacobs <coenjacobs@gmail.com>
2014-10-03 10:17:19 +0200

    Load API key in API handler constructor

--- includes/core/class-wp-api-handler.php
+++ includes/core/class-wp-api-handler.php
@@ -1,6 +1,16 @@
 <?php
 
 class WP_API_Handler implements I_Naguro_API_Handler {
+	private $api_key;
+
+	public function __construct() {
+		$this->api_key = get_option('naguro_api_key', false);
+
+		if ( ! $this->api_key ) {
+			wp_die('No API key provided...');
+		}
+	}
+	
 	public function handle_request( $params = array(), $request = false, $type = 'post' ) {}
 
 	public function set_api_key( $api_key ) {}
