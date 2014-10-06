f6b8cadcd4ecd9d51a268db046d8407fee2903e3
Coen Jacobs <coenjacobs@gmail.com>
2014-10-06 11:19:40 +0200

    Load designer HTML over new API endpoint

--- includes/core/class-wc-naguro-designer.php
+++ includes/core/class-wc-naguro-designer.php
@@ -9,26 +9,33 @@ class WC_Naguro_Designer {
 	/** @var WC_Naguro_Session */
 	private $session;
 
+	/** @var array */
+	private $designer_data;
+
 	/**
 	 * @param $product WC_Product
 	 */
 	public function __construct( $product ) {
 		$this->product = $product;
+		$this->prepare_request();
 		$this->html = $this->get_html();
 
+		// @todo CSS and JS local loading to cache
+		// @todo Remove assets dependency in composer.json
 		wp_enqueue_script('naguro-designer', NAGURO_PLUGIN_URL . 'vendor/radishconcepts/naguro-frontend-assets/js/naguro-designer-nl_NL.js');
 		wp_enqueue_style('naguro-designer', NAGURO_PLUGIN_URL . 'vendor/radishconcepts/naguro-frontend-assets/css/style.css');
 	}
 
-	private function get_html() {
+	private function prepare_request() {
 		$this->session = new WC_Naguro_Session();
 		$this->session->set( 'product_id', $this->product->id);
 
-		// @todo
-		// WC_Naguro_HTML_Get_Request to take over this request
-		// CSS and JS local loading to cache
+		$request = new WC_Naguro_HTML_Get_Request(array( 'session_id' => $this->session->get_id() ) );
+		$this->designer_data = $request->get();
+	}
 
-		$html = file_get_contents( NAGURO_PLUGIN_PATH . 'vendor/radishconcepts/naguro-frontend-assets/build.html');
+	private function get_html() {
+		$html = $this->designer_data['html'];
 		$html = str_replace('{{endpoint}}', get_home_url() . '/wp-admin/admin-ajax.php', $html );
 		$html = str_replace('{{session-id}}', $this->session->get_id(), $html );
 		return $html;
--- includes/core/requests/class-wc-naguro-html-get-request.php
+++ includes/core/requests/class-wc-naguro-html-get-request.php
@@ -1,16 +1,14 @@
 <?php
 
 class WC_Naguro_HTML_Get_Request extends WC_Naguro_Request {
-	public function output() {
+	public function get() {
 		$data = $this->handler->handle_request('get-html', $this->params, 'get' );
 		$body = json_decode( $data['body'] );
 
-		$data_array = array(
+		return array(
 			'html' => $body->html,
 			'css'  => $body->css,
 			'js'   => $body->js,
 		);
-
-		echo json_encode($data_array); die();
 	}
 }
