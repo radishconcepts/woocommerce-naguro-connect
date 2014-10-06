af7a541e24b8bc69b4f12654c2508f0eb2fafcc3
Coen Jacobs <coenjacobs@gmail.com>
2014-10-06 09:19:33 +0200

    WP API handler implementation

--- includes/core/class-wp-api-handler.php
+++ includes/core/class-wp-api-handler.php
@@ -16,8 +16,33 @@ class WP_API_Handler {
 		}
 	}
 
-	public function handle_request( $params = array(), $request = false, $type = 'post' ) {
+	public function handle_request( $endpoint, $params = array(), $type = 'post' ) {
+		$params = $this->add_auth_token_to_header( $params );
+		$url = $this->generate_api_endpoint_url( $endpoint );
+
+		if ( 'get' == $type ) {
+			$data = $this->get_request( $url, $params );
+		} else {
+			$data = $this->post_request( $url, $params );
+		}
+
+		return $data;
 	}
 
-	public function set_api_key( $api_key ) {}
+	private function generate_api_endpoint_url( $endpoint ) {
+		return trailingslashit( trailingslashit( $this->api_url ) . $endpoint );
+	}
+
+	private function add_auth_token_to_header( $params ) {
+		$params['headers']['X-Auth-Token'] = $this->api_key;
+		return $params;
+	}
+
+	private function get_request( $url, $params ) {
+		return wp_remote_get( $url, $params );
+	}
+
+	private function post_request( $url, $params ) {
+		return wp_remote_post( $url, $params );
+	}
 }
