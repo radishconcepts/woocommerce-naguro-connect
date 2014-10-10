4a0835dc7f5af9755b0d4449d3d98f6a30469a2a
Coen Jacobs <coenjacobs@gmail.com>
2014-10-10 10:13:44 +0200

    Post actual height of printable area per design area

--- includes/core/requests/class-wc-naguro-order-preview-get-request.php
+++ includes/core/requests/class-wc-naguro-order-preview-get-request.php
@@ -2,10 +2,20 @@
 
 class WC_Naguro_Order_Preview_Get_Request extends WC_Naguro_Request {
 	public function output() {
-		$data = $this->handler->handle_request('order-preview', $this->params, 'post' );
-		$output_data = json_decode( $data['body'] );
+		$session = new WC_Naguro_Session( $this->params['session'] );
+		$design_areas = get_post_meta( $session->get('product_id'), 'naguro_design_area' );
 
+		$this->params['design_area_array'] = array();
+		foreach ( $design_areas as $key => $design_area ) {
+			$image_src = wp_get_attachment_image_src( $design_area['product_image_id'], 'full' );
+			$height = ( $image_src[2] / 100 ) * $design_area['print_height'];
+			$this->params['design_area_array'][ $key ] = $height;
+		}
+
+		$data = $this->handler->handle_request('order-preview', $this->params, 'post' );
 		$output_array = array();
+
+		$output_data = json_decode( $data['body'] );
 
 		foreach ( $output_data as $part ) {
 			$output_array[] = array(
