d150119d16c8abdcf4529d176cd0f36996e69610
Coen Jacobs <coenjacobs@gmail.com>
2014-10-10 10:20:57 +0200

    Pass width as well, in array with height

--- includes/core/requests/class-wc-naguro-order-preview-get-request.php
+++ includes/core/requests/class-wc-naguro-order-preview-get-request.php
@@ -8,8 +8,13 @@ class WC_Naguro_Order_Preview_Get_Request extends WC_Naguro_Request {
 		$this->params['design_area_array'] = array();
 		foreach ( $design_areas as $key => $design_area ) {
 			$image_src = wp_get_attachment_image_src( $design_area['product_image_id'], 'full' );
+			$width = ( $image_src[1] / 100 ) * $design_area['print_width'];
 			$height = ( $image_src[2] / 100 ) * $design_area['print_height'];
-			$this->params['design_area_array'][ $key ] = $height;
+
+			$this->params['design_area_array'][ $key ] = array(
+				'width' => $width,
+				'height' => $height,
+			);
 		}
 
 		$data = $this->handler->handle_request('order-preview', $this->params, 'post' );
