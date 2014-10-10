c0693ed82c6a6a664bed20ab40aa71dfbfc0c676
Coen Jacobs <coenjacobs@gmail.com>
2014-10-10 09:44:10 +0200

    Order preview endpoint compat

--- includes/core/requests/class-wc-naguro-order-preview-get-request.php
+++ includes/core/requests/class-wc-naguro-order-preview-get-request.php
@@ -5,9 +5,14 @@ class WC_Naguro_Order_Preview_Get_Request extends WC_Naguro_Request {
 		$data = $this->handler->handle_request('order-preview', $this->params, 'post' );
 		$output_data = json_decode( $data['body'] );
 
-		$output_array = array(
-			'src' => $output_data->image,
-		);
+		$output_array = array();
+
+		foreach ( $output_data as $part ) {
+			$output_array[] = array(
+				'src'            => $part->src,
+				'design_area_id' => $part->design_area_id,
+			);
+		}
 
 		echo json_encode($output_array); die();
 	}
