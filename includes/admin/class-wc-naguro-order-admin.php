dafbc0df8d9cc3a65f2d8a1befedace498f74e2f
Coen Jacobs <coenjacobs@gmail.com>
2014-10-20 13:24:26 +0200

    Fixed display of design area images downloads

--- includes/admin/class-wc-naguro-order-admin.php
+++ includes/admin/class-wc-naguro-order-admin.php
@@ -6,18 +6,20 @@ class WC_Naguro_Order_Admin {
 	}
 
 	public function output_line_item( $item_id, $item, $product ) {
-		if ( isset( $item['naguro_session_object'] ) && isset( $session_object['preview'] ) && is_array( $session_object['preview'] ) ) {
+		if ( isset( $item['naguro_session_object'] ) ) {
 			$session_object = unserialize( $item['naguro_session_object'] );
-			$design_areas = get_post_meta( $product->id, 'naguro_design_area' );
+			if ( isset( $session_object['preview'] ) && is_array( $session_object['preview'] ) ) {
+				$design_areas   = get_post_meta( $product->id, 'naguro_design_area' );
 
-			$output_array[] = '<b>Download designs:</b> ';
+				$output_array[] = '<b>Download designs:</b> ';
 
-			foreach ( $session_object['preview'] as $key => $objects ) {
-				$output_array[] = '<a href="'.$objects['src'].'">'. $design_areas[ $key ]['name'] . '</a>';
+				foreach ( $session_object['preview'] as $key => $objects ) {
+					$output_array[] = '<a href="' . $objects['src'] . '">' . $design_areas[ $key ]['name'] . '</a>';
+				}
+
+				echo array_shift( $output_array );
+				echo implode( ', ', $output_array );
 			}
-
-			echo array_shift( $output_array );
-			echo implode( ', ', $output_array );
 		}
 	}
 }
