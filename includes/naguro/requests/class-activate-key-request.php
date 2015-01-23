bb9952f4377443c4d8fd40412af0078a13da1d43
Coen Jacobs <coenjacobs@gmail.com>
2015-01-23 10:21:26 +0100

    Link requests to each other to activate/trial keys

--- includes/naguro/requests/class-activate-key-request.php
+++ includes/naguro/requests/class-activate-key-request.php
@@ -2,7 +2,7 @@
 
 class Naguro_Activate_Key_Request extends Naguro_Request {
 	public function do_request() {
-		$this->handler->handle_request( 'activate-key', $this->params, 'post' );
-		var_dump( $this->handler->get_data() ); die();
+		$this->handler->handle_request( 'key/activate', $this->params, 'post' );
+		return $this->handler->get_data();
 	}
 }
