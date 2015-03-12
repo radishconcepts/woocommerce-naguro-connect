dc4074a231449eba70fc15aa1220dae2680382d2
Gaya <gaya.kessler@gmail.com>
2015-03-12 13:57:37 +0100

    add overlay image data

--- includes/woocommerce/admin/class-wc-naguro-overlay.php
+++ includes/woocommerce/admin/class-wc-naguro-overlay.php
@@ -4,6 +4,8 @@ class WC_Naguro_Overlay {
 	function __construct() {
 		add_action("naguro_woocommerce_before_printable_area_button", array($this, "add_design_area_overlay_upload"));
 		add_action("naguro_woocommerce_after_printable_area_image", array($this, "add_overlay_image"));
+
+		add_filter("naguro_woocommerce_design_area_data", array($this, "handle_design_area_data"));
 	}
 
 	function add_design_area_overlay_upload($rand) {
@@ -26,5 +28,14 @@ class WC_Naguro_Overlay {
 		} else {
 			echo '<img class="overlay-image" src="" />';
 		}
+	}
+
+	function handle_design_area_data($design_area_data) {
+		if ( isset( $design_area_data['product_overlay_id'] ) ) {
+			$image_src = wp_get_attachment_image_src( $design_area_data['product_overlay_id'], 'full' );
+			$design_area_data['product_overlay'] = $image_src[0];
+		}
+
+		return $design_area_data;
 	}
 }
