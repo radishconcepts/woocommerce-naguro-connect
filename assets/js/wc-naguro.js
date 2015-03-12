75ee7d7e55cc8358207376948fc4fe770f386ad5
Gaya <gaya.kessler@gmail.com>
2015-03-12 15:52:20 +0100

    show button again

--- assets/js/wc-naguro.js
+++ assets/js/wc-naguro.js
@@ -175,7 +175,8 @@
         contentBox.css({
             width: "100%",
             height: "100%",
-            padding: "0"
+            padding: "0",
+            overflow: "visible"
         }).append(obj);
 
         contentBox.css({
