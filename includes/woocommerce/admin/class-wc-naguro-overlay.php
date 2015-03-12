7f7a35223314b60b14cfc42ee9dad2e220526ba6
Gaya <gaya.kessler@gmail.com>
2015-03-12 11:18:01 +0100

    add overlay field

--- includes/woocommerce/admin/class-wc-naguro-overlay.php
+++ includes/woocommerce/admin/class-wc-naguro-overlay.php
@@ -2,6 +2,20 @@
 
 class WC_Naguro_Overlay {
 	function __construct() {
+		add_action("naguro_woocommerce_before_printable_area_button", array($this, "add_design_area_overlay_upload"));
+	}
 
+	function add_design_area_overlay_upload($rand) {
+		$name = WC_Naguro::$prefix . "designarea[overlay][" . $rand . "]";
+
+		woocommerce_wp_text_input(array(
+			"id"            => $name,
+			"label"         => "Overlay image",
+			"description"   => "Upload an image that will serve as the overlay image that will display a top of the designer",
+			"name"          => $name,
+			"value"         => "",
+			"class"         => "",
+			"type"          => "file"
+		));
 	}
 }
