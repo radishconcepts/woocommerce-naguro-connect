fdea7c8101cbf2425657c8b39d9c8a1e696388b4
Coen Jacobs <coenjacobs@gmail.com>
2015-03-16 11:32:15 +0100

    Only load active modules

--- includes/naguro/modules/class-modules-loader.php
+++ includes/naguro/modules/class-modules-loader.php
@@ -1,8 +1,22 @@
 <?php
 
 class Naguro_Modules_Loader {
+	private $modules = array(
+		'overlay'
+	);
+
 	public function __construct() {
-		// Needs to check naguro_active_modules to fire up modules
-		new Naguro_Overlay_Module();
+		$modules = apply_filters( 'naguro_available_modules', $this->modules );
+		$active_modules = get_option( 'naguro_active_modules', array() );
+		$active_modules = array_intersect( $modules, $active_modules );
+
+		foreach ( $active_modules as $module ) {
+			$this->load_module( $module );
+		}
+	}
+
+	private function load_module( $module ) {
+		$class_name = 'Naguro_' . ucwords($module) . '_Module';
+		return new $class_name();
 	}
 }
