61c18fff00119d3396dba17a041424aec5e9f8ad
Coen Jacobs <coenjacobs@gmail.com>
2015-04-02 13:15:51 +0200

    Make sure right editor is requested

--- includes/naguro/requests/class-html-get-request.php
+++ includes/naguro/requests/class-html-get-request.php
@@ -7,7 +7,33 @@ class Naguro_HTML_Get_Request extends Naguro_Request {
 
 	public function get() {
 		$product_id = $this->session->get('product_id');
-		$this->handler->handle_request('get-html', $this->params, 'get' );
+		$design_areas = get_post_meta( $product_id, 'naguro_design_area' );
+
+		$use_overlay_module = false;
+
+		foreach ( $design_areas as $design_area ) {
+			if ( ! empty( $design_area['product_overlay_id'] ) && 0 != $design_area['product_overlay_id'] ) {
+				$use_overlay_module = true;
+				break;
+			}
+		}
+
+		if ( $use_overlay_module ) {
+			$url_params['modules'] = 'overlay';
+		} else {
+			$url_params['modules'] = '';
+		}
+
+		$url_params['version'] = '1.1';
+		$url_params['theme'] = 'standard';
+		$url_params['language'] = 'en_GB';
+
+		$endpoint = 'get-html?';
+		foreach ( $url_params as $key => $value ) {
+			$endpoint .= $key .'='. $value.'&';
+		}
+
+		$this->handler->handle_request($endpoint, $this->params, 'get' );
 		$data = $this->handler->get_data();
 		$body = json_decode( $data['body'] );
 
