e4d211974c888752a930927aa6e16aa12c80dafc
Coen Jacobs <coenjacobs@gmail.com>
2015-04-14 15:37:44 +0200

    Make sure files are downloaded the WordPress way

--- includes/woocommerce/core/class-wc-naguro-downloader.php
+++ includes/woocommerce/core/class-wc-naguro-downloader.php
@@ -22,14 +22,41 @@ class WC_Naguro_Downloader {
 			return;
 		}
 
+		include(ABSPATH . 'wp-admin/includes/file.php' );
+
 		$data = $_POST['data'];
 		$store_array = array();
 
-		foreach ( array( 'html' => 'file', 'css' => 'url', 'js' => 'url' ) as $key => $value_to_store ) {
-			$assets_api_url = apply_filters( 'wc_naguro_assets_api_endpoint_url', 'http://api.naguro.com:5000/' );
-			$contents = file_get_contents( $assets_api_url . $data[$key] );
-			$file = $this->save_file_to_disk( $data['hash'], $key, $contents );
-			$store_array[$key] = $file[ $value_to_store ];
+		$type_arr = array( 'html' => 'url', 'css' => 'url', 'js' => 'url' );
+
+		foreach ( $type_arr as $key => $value_to_store ) {
+			$assets_api_url = apply_filters( 'wc_naguro_assets_api_endpoint_url', 'http://api.naguro.com/' );
+			$file_url = $assets_api_url . $data[$key];
+			$temp_file = download_url( $file_url );
+
+			// array based on $_FILE as seen in PHP file uploads
+			$file = array(
+				'name' => basename($file_url),
+				'type' => 'image/png',
+				'tmp_name' => $temp_file,
+				'error' => 0,
+				'size' => filesize($temp_file),
+			);
+
+			$overrides = array(
+				'test_form' => false,
+				'test_size' => true,
+				'test_upload' => true,
+			);
+
+			// move the temporary file into the uploads directory
+			$results = wp_handle_sideload( $file, $overrides );
+
+			if (!empty($results['error'])) {
+				exit;
+			} else {
+				$store_array[$key] = $results[ $value_to_store ];
+			}
 		}
 
 		update_option( 'naguro_editor_' . $data['hash'], $store_array );
@@ -37,10 +64,5 @@ class WC_Naguro_Downloader {
 		remove_filter( 'upload_mimes', array( $this, 'upload_mimes' ) );
 
 		exit;
-	}
-
-	private function save_file_to_disk( $handle, $type, $contents ) {
-		$file = wp_upload_bits( $handle . '.' . $type, null, $contents, date( 'Y/m' ) );
-		return $file;
 	}
 }
