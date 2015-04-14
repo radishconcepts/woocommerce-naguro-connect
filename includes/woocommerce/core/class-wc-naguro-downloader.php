96462ddd7fe780a44e1e48f6d3c4c0c5f2f5efd7
Coen Jacobs <coenjacobs@gmail.com>
2015-04-14 12:12:26 +0200

    Download assets from API

--- includes/woocommerce/core/class-wc-naguro-downloader.php
+++ includes/woocommerce/core/class-wc-naguro-downloader.php
@@ -2,12 +2,45 @@
 
 class WC_Naguro_Downloader {
 	public function __construct() {
-		add_action( 'init', array( $this, 'listener' ) );
+		add_action( 'init', array( $this, 'rewrite_rules' ), 1 );
+		add_action( 'template_redirect', array( $this, 'listener' ) );
+	}
+
+	public function upload_mimes( $types ) {
+		$types['html'] = 'text/html';
+		return $types;
+	}
+
+	public function rewrite_rules() {
+		add_rewrite_endpoint( 'naguro-editor', EP_ROOT );
 	}
 
 	public function listener() {
-		if ( isset( $_GET['naguro_download_editor'] ) ) {
-			// Do download magic here
+		add_filter( 'upload_mimes', array( $this, 'upload_mimes' ) );
+
+		if ( 'download' != get_query_var( 'naguro-editor' ) ) {
+			return;
 		}
+
+		$data = $_POST['data'];
+		$store_array = array();
+
+		foreach ( array( 'html', 'css', 'js' ) as $key ) {
+			$assets_api_url = apply_filters( 'wc_naguro_assets_api_endpoint_url', 'http://api.naguro.com:5000/' );
+			$contents = file_get_contents( $assets_api_url . $data[$key] );
+			$file = $this->save_file_to_disk( $data['hash'], $key, $contents );
+			$store_array[$key] = $file['file'];
+		}
+
+		update_option( 'naguro_editor_' . $data['hash'], $store_array );
+
+		remove_filter( 'upload_mimes', array( $this, 'upload_mimes' ) );
+
+		exit;
+	}
+
+	private function save_file_to_disk( $handle, $type, $contents ) {
+		$file = wp_upload_bits( $handle . '.' . $type, null, $contents, date( 'Y/m' ) );
+		return $file;
 	}
 }
